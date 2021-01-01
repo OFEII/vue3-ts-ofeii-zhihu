629f98e0c8777450f8eb6a343f9e6e02a1d25843
OFEII <2282252066@qq.com>
2021-01-01 23:58:34 +0800

    feat: test ref && toRefs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>{{ cnt }}</h1>
+    <h1>{{ double }}</h1>
+    <button @click="increase">🤙+1</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import { computed, defineComponent, reactive, ref, toRefs } from "vue";
+interface Dataprops {
+  cnt: number;
+  double: number;
+  increase: () => void;
+}
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld
+  setup() {
+    // 2.base Reactive 函数 
+    const data: Dataprops = reactive({
+      cnt: 0,
+      increase: () => {
+        data.cnt++;
+      },
+      double: computed(() => data.cnt * 2)
+    });
+    const refData = toRefs(data);
+    return {
+      ...refData
+    };
+    // 1：base ref syntax
+    // const cnt = ref(0);
+    // const double = computed(() => {
+    //   return cnt.value * 2;
+    // });
+    // const increase = () => {
+    //   cnt.value++;
+    // };
+    // return {
+    //   cnt,
+    //   increase,
+    //   double
+    // };
   }
 });
 </script>
