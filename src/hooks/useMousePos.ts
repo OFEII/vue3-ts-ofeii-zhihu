e1e57738ebb211f7a6835336e3644887b257c24e
OFEII <2282252066@qq.com>
2021-01-02 13:56:42 +0800

    fix: 3.10-test vue3 hooks mouse trace model base reactive

--- src/hooks/useMousePos.ts
+++ src/hooks/useMousePos.ts
@@ -7,24 +7,47 @@ import {
   watch,
   onMounted,
   onUnmounted
-} from "vue";
+} from "vue"
 
+interface MousePos {
+  x: number
+  y: number
+}
 
 function useMousePos() {
-  // mouse trace model
-  const x = ref(0);
-  const y = ref(0);
+  // mouse trace model-reactive
+  const mouseData: MousePos = reactive({
+    x: 0,
+    y: 0
+  })
   const updateMouse = (e: MouseEvent) => {
-    x.value = e.pageX;
-    y.value = e.pageY;
+    mouseData.x = e.pageX;
+    mouseData.y = e.pageY;
   };
   onMounted(() => {
-    document.addEventListener("click", updateMouse);
+    document.addEventListener("click", updateMouse)
   });
   onUnmounted(() => {
-    document.removeEventListener("click", updateMouse);
+    document.removeEventListener("click", updateMouse)
   })
-  return {x, y}
+  const mouseRefData = toRefs(mouseData)
+  return {
+    ...mouseRefData
+  }
+  // mouse trace model-ref
+  // const x = ref(0);
+  // const y = ref(0);
+  // const updateMouse = (e: MouseEvent) => {
+  //   x.value = e.pageX;
+  //   y.value = e.pageY;
+  // };
+  // onMounted(() => {
+  //   document.addEventListener("click", updateMouse);
+  // });
+  // onUnmounted(() => {
+  //   document.removeEventListener("click", updateMouse);
+  // })
+  // return {x, y}
 }
 
 export default useMousePos
