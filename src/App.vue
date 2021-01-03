8fcb54ae5dc468b708db825dd73f79fdf2bcfd84
OFEII <2282252066@qq.com>
2021-01-03 14:06:21 +0800

    feat: +component-columnlist base

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+    <column-list :list="list"></column-list>
+    <column-list :list="list"></column-list>
   </div>
-  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
-}
+]
+export default defineComponent({
+  name: 'App',
+  setup (props) {
+    return {
+      list: testData
+    }
+  },
+  components: {
+    ColumnList
+  }
+})
+</script>
+
+<style lang="scss">
 </style>
---
+++ src/views/TestVue3.vue
@@ -0,0 +1,126 @@
+<template>
+  <Suspense>
+    <template #default>
+      <div>
+        <async-show />
+        <dog-show />
+      </div>
+    </template>
+    <template #fallback>
+      <h1>susupense-laoding!!!</h1>
+    </template>
+  </Suspense>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1 v-if="loading">Loading!...</h1>
+    <!-- <img v-if="loader" :src="res[0].url" /> -->
+    <modal :isOpen="modalIsOpen" @close-modal="onModalClose" />
+    <button @click="openModal">🤡</button>
+    <h1>X:{{ x }} Y:{{ y }}</h1>
+    <h1>Title: {{ greetings }}</h1>
+    <h1>{{ cnt }}</h1>
+    <h1>{{ double }}</h1>
+    <ul>
+      <li v-for="n in numberArr" :key="n">
+        <h1>{{ n }}</h1>
+      </li>
+      <h1>{{ person.name }}</h1>
+    </ul>
+    <button @click="increase">🤙+1</button>
+    <button @click="updateGreeting">✍update title</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, reactive, ref, toRefs, watch } from 'vue'
+
+import useMousePos from '@/hooks/useMousePos'
+import useUrlLoader from '@/hooks/useUrlLoader'
+import Modal from '@/components/Modal.vue'
+import AsyncShow from '@/components/AsyncShow.vue'
+import DogShow from '@/components/DogShow.vue'
+const a = '12'
+interface Dataprops {
+  cnt: number;
+  double: number;
+  increase: () => void;
+  numberArr: number[];
+  person: { name?: string };
+}
+interface DogRes {
+  message: string;
+  status: string;
+}
+
+interface CatRes {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+export default defineComponent({
+  name: 'Home',
+  setup () {
+    // 2.base Reactive 函数
+    const data: Dataprops = reactive({
+      cnt: 0,
+      increase: () => {
+        data.cnt++
+      },
+      double: computed(() => data.cnt * 2),
+      numberArr: [0, 1, 2],
+      person: {}
+    })
+    data.numberArr[0] = 5
+    data.person.name = 'ofeii'
+    // useUrlLoader
+    const { res, loading, loader } = useUrlLoader<CatRes[]>(
+      'https://api.thecatapi.com/v1/images/search?limit=1'
+    )
+    // const { res, loading, loader } = useUrlLoader<DogRes[]>(
+    //   "https://dog.ceo/api/breeds/image/random"
+    // );
+    // useMousePos
+    const { x, y } = useMousePos()
+    // watch test
+    const greetings = ref('')
+    const updateGreeting = () => {
+      greetings.value += 'OFEII!'
+    }
+    watch([greetings, () => data.cnt, res], (newVal, oldVal) => {
+      if (res.value) {
+        console.log('value', res.value[0].url)
+      }
+      console.log('new', newVal)
+      console.log('old', oldVal)
+      document.title = `updated ${greetings.value}`
+    })
+    const refData = toRefs(data)
+    const modalIsOpen = ref(false)
+    const openModal = () => {
+      modalIsOpen.value = true
+    }
+    const onModalClose = () => {
+      modalIsOpen.value = false
+    }
+    return {
+      ...refData,
+      greetings,
+      updateGreeting,
+      x,
+      y,
+      res,
+      loading,
+      loader,
+      openModal,
+      onModalClose,
+      modalIsOpen
+    }
+  },
+  components: {
+    Modal,
+    AsyncShow,
+    DogShow
+  }
+})
+</script>
