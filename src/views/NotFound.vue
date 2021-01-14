712d26bfeea6d9f62c6eb7056fbca05cece9bff6
OFEII <2282252066@qq.com>
2021-01-14 19:44:19 +0800

    feat: + views - NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    NotFound 404
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'NotFound'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
