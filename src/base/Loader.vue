ce3c4ef026ef37d11dc1436f05cc5150dd3b3868
OFEII <2282252066@qq.com>
2021-01-14 18:00:41 +0800

    feat: +base components - loader.vue

---
+++ src/base/Loader.vue
@@ -0,0 +1,52 @@
+<template>
+  <teleport to="#back">
+    <div
+      class="d-flex justify-content-center align-items-center h-100 w-100 loading-container"
+      :style="{backgroundColor: background || ''}"
+    >
+      <div class="loading-content">
+        <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden sr-only">{{ text || 'loading' }}</span>
+        </div>
+        <p v-if="text" class="text-primary small">{{ text }}</p>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import useDOMCreate from '@/hooks/useDOMCreate'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Loader',
+  props: {
+    text: {
+      type: String
+    },
+    background: {
+      type: String
+    }
+  },
+  setup () {
+    useDOMCreate('back')
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.loading-container {
+  background: rgba(255, 255, 255, .5);
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  .loading-content {
+    text-align: center;
+  }
+}
+
+</style>
