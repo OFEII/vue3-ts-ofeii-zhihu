0d5bb5b08919eae4412e133a65ad1d66ed25a77e
OFEII <2282252066@qq.com>
2021-01-14 19:40:30 +0800

    feat: +libs - storage.ts

---
+++ src/libs/storage.ts
@@ -0,0 +1,48 @@
+export enum StorageType {
+  Local = 'Local',
+  Session = 'Session'
+}
+
+export class StorageHandler {
+  // store storage
+  setItem (type: StorageType, key: string, value: string) {
+    if (type === StorageType.Local) {
+      localStorage.setItem(key, value)
+    } else {
+      sessionStorage.setItem(key, value)
+    }
+  }
+
+  // get storage
+  getItem (type: StorageType, key: string) {
+    if (type === StorageType.Local) {
+      if (localStorage.getItem(key)) {
+        return localStorage.getItem(key)
+      }
+    } else {
+      if (sessionStorage.getItem(key)) {
+        return sessionStorage.getItem(key)
+      }
+    }
+  }
+
+  // remove storage
+  remove (type: StorageType, key: string) {
+    if (type === StorageType.Local) {
+      if (localStorage.getItem(key)) localStorage.removeItem(key)
+    } else {
+      if (sessionStorage.getItem(key)) sessionStorage.removeItem(key)
+    }
+  }
+
+  // clear
+  clear (type: StorageType) {
+    if (type === StorageType.Local) {
+      localStorage.clear()
+    } else {
+      sessionStorage.clear()
+    }
+  }
+}
+
+export const storageType = StorageType.Local
