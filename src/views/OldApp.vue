5cff08398d9341d7bf813d9d180cccc433c03c9c
OFEII <2282252066@qq.com>
2021-01-14 19:44:31 +0800

    feat: + views - TestApp.vue

---
+++ src/views/OldApp.vue
@@ -0,0 +1,122 @@
+<template>
+  <global-header :user="currentUser"></global-header>
+  <div class="container">
+    <validate-form @form-submit="onFormSumbit">
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input
+          :rules="emailRules"
+          v-model="emailVal"
+          type="text"
+          placeholder="请输入邮箱地址">
+        </validate-input>
+      </div>
+      <div class="mb-3">
+      <label class="form-label">密码</label>
+        <validate-input
+          :rules="passwordRules"
+          v-model="passwordVal"
+          type="password"
+          placeholder="请输入密码">
+        </validate-input>
+      </div>
+      <template #submit>
+        <span class="btn btn-danger">Submit</span>
+      </template>
+    </validate-form>
+    <column-list :list="list"></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+// import { defineComponent, reactive, ref } from 'vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import { ColumnProps, UserProps, RulesProp } from '../store/types'
+// import ColumnList from './components/ColumnList.vue'
+// import GlobalHeader from './components/GlobalHeader.vue'
+// import ValidateInput from './components/ValidateInput.vue'
+// import ValidateForm from './components/ValidateForm.vue'
+// const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+// const currentUser: UserProps = {
+//   isLogin: true,
+//   nickName: 'OFEII'
+// }
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3092529483,2095048025&fm=26&gp=0.jpg'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2400920312,4079163394&fm=26&gp=0.jpg'
+//   },
+//   {
+//     id: 3,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3107801461,4048012422&fm=26&gp=0.jpg'
+//   },
+//   {
+//     id: 4,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3796312028,1081481082&fm=26&gp=0.jpg'
+//   }
+// ]
+// export default defineComponent({
+//   name: 'App',
+//   setup (props) {
+//     // const inputRef = ref<any>()
+//     const emailVal = ref('123@test.com')
+//     const passwordVal = ref('123')
+//     const emailRules: RulesProp = [
+//       { type: 'required', message: '电子邮箱地址不能为空' },
+//       { type: 'email', message: '请输入正确的电子邮箱格式' }
+//     ]
+//     const passwordRules: RulesProp = [
+//       { type: 'required', message: '密码不能为空' }
+//     ]
+//     const onFormSumbit = (result: boolean) => {
+//       console.log('res', result)
+//     }
+//     const emailRef = reactive({
+//       val: '',
+//       error: false,
+//       message: ''
+//     })
+//     const validateEmail = () => {
+//       if (emailRef.val.trim() === '') {
+//         emailRef.error = true
+//         emailRef.message = 'can not be empty'
+//       } else if (!emailReg.test(emailRef.val)) {
+//         emailRef.error = true
+//         emailRef.message = 'should be valid email'
+//       }
+//     }
+//     return {
+//       list: testData,
+//       currentUser: currentUser,
+//       emailRef,
+//       validateEmail,
+//       emailRules,
+//       passwordRules,
+//       emailVal,
+//       passwordVal,
+//       onFormSumbit
+//     }
+//   },
+//   components: {
+//     ColumnList,
+//     GlobalHeader,
+//     ValidateInput,
+//     ValidateForm
+//   }
+// })
+</script>
+
+<style lang="scss">
+</style>
